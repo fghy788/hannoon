c63ec8270490ca0a53bfe534744ae1b94a3cd060
KimBab <fghy788@gmail.com>
2025-06-15 03:20:07 +0900

    서버 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,8 +62,14 @@ export default function Home() {
     };
   };
 
-  const toUtcTime = (ts: any) => {
-    return new Date(ts * 1000).toISOString().substring(11, 16);
+  const toUtcTime = (ts: number) => {
+    const date = new Date(ts * 1000);
+
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const hour = date.getUTCHours().toString().padStart(2, "0");
+    const minute = date.getUTCMinutes().toString().padStart(2, "0");
+
+    return `${day}:${hour}:${minute}`;
   };
 
   const convertTimestampsToUnix = (arr: any[]) =>
@@ -77,7 +83,8 @@ export default function Home() {
       const res = await fetch("https://data.spone.fun/search?sort=kesoonho&id=data");
       if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
       const data = await res.json();
-      setData(convertTimestampsToUnix(data));
+      console.log(data);
+      setData(convertTimestampsToUnix(data[0].data.stage2));
     } catch (err) {
       console.error("Fetch error:", err);
       setData([]);
@@ -93,71 +100,82 @@ export default function Home() {
   return (
     <div className={`h-dvh w-dvw`}>
       <main className="flex h-full w-full flex-col gap-2 overflow-y-scroll p-2">
-        <div className={`font-key flex h-12 w-full items-center justify-between rounded-xl border border-black px-4`}>
-          <div className={`flex h-full items-center gap-4`}>
-            <Image src="/slogo.svg" width={30} height={30} alt="logo" />
-            <span className="text-xl">Succinct</span>
-          </div>
-          <Link href={"https://x.com/kesoonho"}>X: @kesoonho</Link>
-        </div>
-
-        <div className={`font-key flex flex-col gap-1 rounded-xl border p-4`}>
-          <div className={`flex h-8 w-full items-center gap-2 rounded-xl px-2`}>
-            <button onClick={() => setMode(0)} className={`${mode === 0 ? "text-pink-400" : "text-black"} cursor-pointer`}>
-              Hourly
-            </button>
-            <button onClick={() => setMode(1)} className={`${mode === 1 ? "text-pink-400" : "text-black"} cursor-pointer`}>
-              Daily
-            </button>
-          </div>
-
-          <div className={`grid h-16 w-full grid-cols-2 gap-2 overflow-x-scroll rounded-xl border px-2 break-keep`}>
-            <div className={`flex h-full w-full items-center justify-start gap-4 text-xs md:text-base`}>
-              <div className={`flex w-12 flex-col items-center`}>
-                <span>Bids</span>
-                <span>({mode === 0 ? "1h" : "24h"})</span>
+        {data.length <= 0 ? (
+          <span>Loading...</span>
+        ) : (
+          <>
+            <div className={`font-key flex h-12 w-full items-center justify-between rounded-xl border border-black px-4`}>
+              <div className={`flex h-full items-center gap-4`}>
+                <Image src="/slogo.svg" width={30} height={30} alt="logo" />
+                <span className="text-xl">Succinct</span>
               </div>
-              <span>{getRate().bids}</span>
-              <span className={`${getChangeRate().bidsRate >= 0 ? "text-green-600" : "text-red-600"}`}>{`${getChangeRate().bidsRate >= 0 ? "+" : "-"}${getChangeRate().bidsRate.toFixed(2)}`}</span>
+              <Link href={"https://x.com/kesoonho"}>X: @kesoonho</Link>
             </div>
 
-            <div className={`flex h-full w-full items-center justify-start gap-4 text-xs md:text-base`}>
-              <div className={`flex w-12 flex-col items-center`}>
-                <span>Points</span>
-                <span>({mode === 0 ? "1h" : "24h"})</span>
+            <div className={`font-key flex flex-col gap-1 rounded-xl border p-4`}>
+              <div className={`flex h-8 w-full items-center gap-2 rounded-xl px-2`}>
+                <button onClick={() => setMode(0)} className={`${mode === 0 ? "text-pink-400" : "text-black"} cursor-pointer`}>
+                  Hourly
+                </button>
+                <button onClick={() => setMode(1)} className={`${mode === 1 ? "text-pink-400" : "text-black"} cursor-pointer`}>
+                  Daily
+                </button>
               </div>
-              <span>{getRate().points}</span>
-              <span className={`${getChangeRate().pointsRate >= 0 ? "text-green-600" : "text-red-600"}`}>{`${getChangeRate().pointsRate >= 0 ? "+" : "-"}${getChangeRate().pointsRate.toFixed(2)}`}</span>
-            </div>
-          </div>
-        </div>
 
-        <div className={`font-key w-full rounded-xl border p-2`}>
-          <div className={`flex h-8 w-full items-center gap-2 rounded-xl px-2`}>
-            <span>Last 5 Contest</span>
-          </div>
-          <div className={`flex h-fit w-full flex-col overflow-y-scroll rounded-xl border px-2 text-xs md:text-base`}>
-            <div className={`grid w-full grid-cols-5 place-items-center`}>
-              <span>Time</span>
-              <span className={`text-green-400`}>Bids</span>
-              <span className={`text-pink-400`}>Stars</span>
-              <span className={`text-yellow-400`}>Multiplier</span>
-              <span className={`text-red-400`}>Prize</span>
-            </div>
-            <hr></hr>
-            {data.slice(0, 5).map((item, index) => {
-              return (
-                <div key={index} className={`grid w-full grid-cols-5 place-items-center`}>
-                  <span>{`[${toUtcTime(item.Timestamp)}]`}</span>
-                  <span className={`text-green-400`}>{item.Bids}</span>
-                  <span className={`text-pink-400`}>{item.Star_Pool}</span>
-                  <span className={`text-yellow-400`}>{item.Multiplier}</span>
-                  <span className={`text-red-400`}>{item.Prize}</span>
+              <div className={`grid h-16 w-full grid-cols-2 gap-2 overflow-x-scroll rounded-xl border px-2 break-keep`}>
+                <div className={`flex h-full w-full items-center justify-start gap-4 text-xs md:text-base`}>
+                  <div className={`flex w-12 flex-col items-center`}>
+                    <span>Bids</span>
+                    <span>({mode === 0 ? "1h" : "24h"})</span>
+                  </div>
+                  <span>{getRate().bids}</span>
+                  <span className={`${getChangeRate().bidsRate >= 0 ? "text-green-600" : "text-red-600"}`}>{`${getChangeRate().bidsRate.toFixed(2)}%`}</span>
                 </div>
-              );
-            })}
-          </div>
-        </div>
+
+                <div className={`flex h-full w-full items-center justify-start gap-4 text-xs md:text-base`}>
+                  <div className={`flex w-12 flex-col items-center`}>
+                    <span>Points</span>
+                    <span>({mode === 0 ? "1h" : "24h"})</span>
+                  </div>
+                  <span>{getRate().points}</span>
+                  <span className={`${getChangeRate().pointsRate >= 0 ? "text-green-600" : "text-red-600"}`}>{`${getChangeRate().pointsRate.toFixed(2)}%`}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className={`font-key w-full rounded-xl border p-2`}>
+              <div className={`flex h-8 w-full items-center gap-2 rounded-xl px-2`}>
+                <span>Last 5 Contest</span>
+              </div>
+              <div className={`flex h-fit w-full flex-col rounded-xl border px-2 text-xs md:text-base`}>
+                <div className={`grid w-full grid-cols-5 place-items-center`}>
+                  <span>Time</span>
+                  <span className={`text-green-400`}>Bids</span>
+                  <span className={`text-pink-400`}>Stars</span>
+                  <span className={`text-yellow-400`}>Multiplier</span>
+                  <span className={`text-red-400`}>Prize</span>
+                </div>
+                <hr></hr>
+                <div className={`flex h-52 flex-col overflow-y-scroll`}>
+                  {data
+                    .slice()
+                    .reverse()
+                    .map((item, index) => {
+                      return (
+                        <div key={index} className={`grid w-full grid-cols-5 place-items-center`}>
+                          <span>{`[${toUtcTime(item.Timestamp)}]`}</span>
+                          <span className={`text-green-400`}>{item.Bids}</span>
+                          <span className={`text-pink-400`}>{item.Star_Pool}</span>
+                          <span className={`text-yellow-400`}>{item.Multiplier}</span>
+                          <span className={`text-red-400`}>{item.Prize}</span>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
